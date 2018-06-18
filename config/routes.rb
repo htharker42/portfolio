8bad0ad28d4239a67dc7844768550bc6bf3d8dcd
htharker42 <htharker@gmail.com>
2018-06-17 20:34:01 -0700

    created comment broadcast - configed websocket route, updated model with a callback

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :comments
+  #resources :comments
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   
   resources :portos, except: [:show] do 
@@ -21,6 +21,8 @@ Rails.application.routes.draw do
 
   end
 
+  mount ActionCable.server => '/cable'
+  
 
   root to: 'pages#home'
 
