850b78f3c4ca0014e6c5148a78951ee67fddc07a
htharker42 <htharker@gmail.com>
2018-06-18 14:25:40 -0700

    fixed bug with draft bugs so they can only be seen by site admins

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -15,13 +15,19 @@ class BlogsController < ApplicationController
   end
 
   def show
-    @blog = Blog.includes(:comments).friendly.find(params[:id])
-    @comment = Comment.new
+  	if logged_in?(:admin) || @blog.published? #blog enum 
+	    @blog = Blog.includes(:comments).friendly.find(params[:id])
+	    @comment = Comment.new
 
-    @page_title = @blog.title
-    @seo_keywords = @blog.body
+	    @page_title = @blog.title
+	    @seo_keywords = @blog.body
+	else
+		redirect_to blogs_path, notice: "You are not authorized to access this page"
+	end 
+			
+	end
 
-  end
+
 
   # GET /blogs/new
   def new
