01e20318d4e4130dba82a0a54db29588c1ae8361
htharker42 <htharker@gmail.com>
2018-06-25 15:46:19 -0700

    reordered javascrpt loading order to fix bugs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,10 +13,10 @@
 //= require jquery
 //= require rails-ujs
 //= require jquery-ui
-//= require bootstrap-sprockets
 //= require html.sortable
 //= require cocoon
 //= require gritter
 //= require cable
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
